5e699005105a53598343dac4c90df92f9d62bd76
Ryan Graessel <rkg8fb@gmail.com>
2016-02-08 16:16:58 -0500

    added 5 homework assignments

---
+++ projects/git_init/goodmeasure_homework/quick_fox.rb
@@ -0,0 +1,12 @@
+class QuickFox
+
+  def jumped_over(lazy_dog, daisy_log)
+  end
+end
+
+quick_fox = QuickFox.new
+lazy_dog = "lazy_dog"
+daisy_log = "daisy_log"
+puts quick_fox.jumped_over(lazy_dog, daisy_log)
+
+quick_fox.jumped_over(lazy_dog, daisy_log)
